f911a6237bfd548e497d25eb361cc7255a294d50
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-16 16:24:52 -0300

    :iphone: style:

---
+++ src/pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import styles from '../../components/components.module.css';
+
+export default function Favorites() {
+  return (<>
+    <Header/>
+    <main className={styles.mainfavorite}>
+
+    </main>
+  </>);
+}
