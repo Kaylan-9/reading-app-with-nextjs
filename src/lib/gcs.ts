ebd91eca661c432f69d944f660104cd20d6ef8bd
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-25 15:39:03 -0300

    :sparkles: feat:

--- src/lib/gcs.ts
+++ src/lib/gcs.ts
@@ -1,14 +1,13 @@
 import { Storage } from "@google-cloud/storage";
 
+const serviceAccountKey: string = process.env.CREDENTIALS_CLOUD || '';
 const storage = new Storage({
-    keyFilename: "KEY-BOOKCLOUD.json",
+  credentials: JSON.parse(serviceAccountKey)
 });
 
 const bucket = storage.bucket(process.env.GCS_BUCKET as string);
-
 export const createWriteStream = (filename: string, contentType?: string) => {
   const ref = bucket.file(filename);
-
   const stream = ref.createWriteStream({
     gzip: true,
     contentType: contentType,
