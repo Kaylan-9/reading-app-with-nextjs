4272563e1c06395050188b0413a2d49c2d91c8bc
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-03-13 16:18:04 -0300

    :sparkles: feat: adding code to display contedding code to display content referring to the author in an orderly and optimized way

--- src/lib/db/users.ts
+++ src/lib/db/users.ts
@@ -1,3 +1,4 @@
+import { IBookCategories } from "@/types/data/Books";
 import { IUser, IUserBookCategoriesPublic } from "@/types/data/Users";
 import { Book } from "@prisma/client";
 import prisma from "./prisma";
@@ -53,42 +54,47 @@ export async function getUserBooks(id: string) {
 }
 
 export async function getRandomUsersBooks() {
-  const nUsers= await prisma.user.count();
   let 
     data: IUserBookCategoriesPublic[]= [],
-    count= 0,
-    booksCount= 0;
-  while(data.length<3 || count<8) {
+    currentUser= 0,
+    booksCount= 0,
+    attemptsCount= 0,
+    booksN= await prisma.book.count(),
+    usersN= await prisma.user.count(),
+    bookIDs: number[]= [],
+    userIDs: string[]= [];
+  while((data.length<3 && usersN>=3 && userIDs.length<usersN && attemptsCount<10) || (data.length<usersN && usersN<3)) {
     const newItem= (await prisma.user.findMany({
+      where: {
+        id: { notIn: userIDs },
+      },
       include: {
         book: {
           include: {
-            imagepaths: {
-              take: 3,
-              skip: 0  
-            },
+            imagepaths: {take: 3, skip: 0},
             categorie: true
           },
-          take: 3,
-          skip: 0    
+          take: 3, skip: 0    
         }     
       },
       take: 1,
-      skip: Math.floor(Math.random() * nUsers)
+      skip: Math.floor(Math.random() * usersN)
     }))[0] as any;
-    if(!data.some((item: IUserBookCategoriesPublic) => item.id===newItem.id)){
-      data.push(newItem as any);
+    if(newItem!==null && newItem?.id!==undefined) {
+      userIDs.push(newItem?.id);
+      if(newItem.book.length>=3 && !data.some((item: IUserBookCategoriesPublic) => item.id===newItem?.id)){
+        data.push(newItem as any);
+      }
     }
-    count++;
+    attemptsCount++;
   }
-  count= 0;
-  data.map(userData=> userData.book.map(_=> {
+  attemptsCount= 0;
+  data.map(userData=> userData.book.map(book=> {
+    bookIDs.push(book.id);
     booksCount++;
   }));
-  while(data.length<3 && booksCount!=10 && count<(data.length-1)) {
-    const 
-      userID= data[count].id,
-      bookIDs= data[count].book.map(book=> book.id);
+  while(((booksCount<10 && booksN>=10) || (booksCount<booksN && booksN<10)) && attemptsCount<(booksCount*2)) {
+    const userID= data[currentUser]?.id;
     const newItem= (await prisma.book.findFirst({
       where: {
         id: { notIn: bookIDs },
@@ -105,10 +111,13 @@ export async function getRandomUsersBooks() {
       },
     }));
     if(newItem!==null) {
-      data[count].book= [...data[count].book, newItem] as any;
+      data[currentUser].book= [...data[currentUser]?.book, newItem] as any;
+      console.log(newItem);
+      bookIDs.push(newItem.id);
       booksCount++;
-      if(count<(data.length-1)) count++;
+      currentUser= currentUser<data.length-1 ? currentUser+1 : 0;
     }
+    attemptsCount++;
   }
   return data;
 }
